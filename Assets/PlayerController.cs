ab18e27e304bd0b30e821cfe9fb8d6e464e22573
DESKTOP\SeolRang <angelace1007@gmail.com>
2022-10-25 19:19:16 +0900

    add player animation

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rigidbody2D;
+    new Rigidbody2D rigidbody2D;
+    Animator animator;
     float moveSpeed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rigidbody2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("dirX", x);
+        animator.SetFloat("dirY", y);
+        rigidbody2D.velocity = new Vector2(x, y) * moveSpeed;
+        animator.SetBool("isMove",rigidbody2D.velocity.magnitude > 0);
     }
 }
